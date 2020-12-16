6a2dc8bf48694ebf3c9b6b8c8b6d47b16b81d21f
Kha Nguyen <khanguyen1000@gmail.com>
2020-12-16 17:17:53 +0700

    update

--- LKmovie/src/app/user-module/home-page/home-page.component.ts
+++ LKmovie/src/app/user-module/home-page/home-page.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class HomePageComponent implements OnInit {
   constructor() {}
--- LKmovie/src/app/user-module/showtime-movie/showtime-movie.component.ts
+++ LKmovie/src/app/user-module/showtime-movie/showtime-movie.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-showtime-movie',
   templateUrl: './showtime-movie.component.html',
-  styleUrls: ['./showtime-movie.component.scss']
+  styleUrls: ['./showtime-movie.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class ShowtimeMovieComponent implements OnInit {
+  isTab: string = 'MON';
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  handleChangeTab(tab: string): void {
+    this.isTab = tab;
   }
-
 }
